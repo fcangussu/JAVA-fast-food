016012d4cb5a81af492c59c9b9dd1ff199664ad0
Fernando Cangussu <fcangussu@gmail.com>
2023-10-23 21:42:24 -0300

    Update ClienteApplication.java
    TEST SONAR

--- src/main/java/com/fiap/ClienteApplication.java
+++ src/main/java/com/fiap/ClienteApplication.java
@@ -12,6 +12,11 @@ public class ClienteApplication {
         SpringApplication.run(ClienteApplication.class, args);
 //        System.out.println("Hello world!");
 
+        //test sonar Cloud
+        if (1==1) && (3<4) && (10>3){
+            System.out.println("TRUE FOR TESTING ...");
+        }
+            
         //EntityManagerFactory emf = Persistence.createEntityManagerFactory("FastFoodUnit");
         //emf.close();
     }
